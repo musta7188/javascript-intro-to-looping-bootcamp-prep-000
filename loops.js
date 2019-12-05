a7cf967d05008f6fdcefe39616f295c3af332152
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-05 11:11:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,15 +2,16 @@
 
 function forLoop(array){
 
-  for (var i = 0; i < 25; i++){
+  for (let var i = 0; i < 25; i++){
+    
+    if (i === 1){
+      return `I am ${i} strange loop`
 
+    } else {
+      return `I am ${i stange loops}`
+
+    }
 
   }
-  if (i === 1){
-    return `I am ${i} strange loop`
 
-  } else{
-    return `I am ${i stange loops}`
-
-  }
 }
